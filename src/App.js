2fe8d172627aaddf722ba972031d6b932160a50c
CalAnizoba <obinnaanizoba5@gmail.com>
2022-08-11 01:03:19 +0800

    Added routers to help navigate pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import React from 'react';
+import Nav from './navPage';
+import HomePage from '../src/components/HomePage/HomePage';
+import ShopPage from '../src/components/ShopPage/shopPage';
+import CartPage from '../src/components/CartPage/cartPage';
+import ContactPage from '../src/components/ContactPage/ContactPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Nav/>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/Home' element={<HomePage />} />
+        <Route path='/Shop' element={<ShopPage />} />
+        <Route path='/Cart' element={<CartPage />} />
+        <Route path='/Contact' element={<ContactPage />} />
+      </Routes>
+    </Router>
   );
 }
 
