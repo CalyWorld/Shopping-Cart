298445645221aff04490da5fd9e21629d1988925
CalAnizoba <obinnaanizoba5@gmail.com>
2022-08-17 07:06:15 +0800

    Updates

---
+++ precache-manifest.ff02e239f5b89c155db37690c4f10ee6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "83f77863f11cc6f33f7b",
+    "url": "/Shopping-Cart/static/css/main.ebd09a81.chunk.css"
+  },
+  {
+    "revision": "83f77863f11cc6f33f7b",
+    "url": "/Shopping-Cart/static/js/main.83f77863.chunk.js"
+  },
+  {
+    "revision": "291b620dec886f59129f",
+    "url": "/Shopping-Cart/static/js/1.291b620d.chunk.js"
+  },
+  {
+    "revision": "c3d4aa76f36cc5f07643",
+    "url": "/Shopping-Cart/static/js/runtime~main.c3d4aa76.js"
+  },
+  {
+    "revision": "4c7f98be2bc3e70d9af5bc499ee130ed",
+    "url": "/Shopping-Cart/index.html"
+  }
+];
